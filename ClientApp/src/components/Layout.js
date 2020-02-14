6cfb77168e164d2c8b6670c9f4bc0e5093b4c63a
Espen Gronli <espen@efficio-progressio.com>
2020-02-14 13:42:29 +0100

    #4 Change layout to allow for patient list to span entire width.

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Row,Col } from 'reactstrap';
+import { Container } from '@material-ui/core';
 import { NavMenu } from './NavMenu';
 
 export class Layout extends Component {
@@ -9,13 +9,9 @@ export class Layout extends Component {
     return (
       <div>
             <NavMenu />
-            <Container fluid={true}>
-                <Row>
-                    <Col md={{span: 4, offset: 2 }}>
-                        {this.props.children}
-                    </Col>
-                </Row>
-        </Container>
+            <Container>
+                {this.props.children}
+            </Container>
       </div>
     );
   }
